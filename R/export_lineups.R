bb35b46a76780142a6da88376f13acf791432907
Jim <jim.kloet@gmail.com>
2018-02-10 07:41:43 -0600

    update export_lineups.R for more informative filename output and better handling of mismatches in rows

--- R/export_lineups.R
+++ R/export_lineups.R
@@ -2,24 +2,34 @@ export_lineups <- function(lineups,
                            entries, 
                            site = "draftkings", 
                            sport = "nba",
-                           randomize_entries = TRUE,
+                           slate = NULL,
+                           max_lineups = 500,
+                           randomize_entries = FALSE,
                            file_name = paste0(site, "_", 
                                               sport, "_", 
-                                              gsub("[^[:alnum:]]", "", Sys.time()), ".csv")) {
+                                              gsub("[^[:alnum:]]", "", Sys.Date()), "_",
+                                              slate, ".csv")) {
   
   library(dplyr)
 
   cols_to_import <- lineups %>%
     select(ends_with("salary_id"))
   
-  lu_rows <- nrow(cols_to_import)
+  num_import_rows <- nrow(cols_to_import)
   entry_rows <- nrow(entries)
   
-  if (lu_rows < entry_rows) {
-    nrows_to_add <- entry_rows - lu_rows
+  if (num_import_rows < entry_rows) {
+    nrows_to_add <- entry_rows - num_import_rows
     temp <- rbind(cols_to_import, cols_to_import[1:nrows_to_add, ])
+    # TODO: update to include up to 500 lineups
+
     cols_to_import <- temp
   }
+  
+  if (num_import_rows > entry_rows) {
+    cols_to_import <- cols_to_import[c(1:entry_rows), ]
+  }
+  
   
   fixed_names <- gsub("_salary_id", "", names(cols_to_import)) %>%
     toupper()
--- TEST/TEST_RUN.R
+++ TEST/TEST_RUN.R
@@ -6,7 +6,7 @@ for (f in list.files(pattern = "*.R")) {
 rm(list = setdiff(ls(), lsf.str()))
 
 t1 <- Sys.time()
-salaries <- import_salaries("C:/Users/Jim/Documents/dfs/dk/nba/inputs/DKSalaries_20180209_night.csv", 
+salaries <- import_salaries("C:/Users/Jim/Documents/dfs/dk/nba/inputs/DKSalaries_20180210_night.csv", 
                             from_entry = TRUE)
 salary_lu <- make_salary_lu(salaries, from_entry = TRUE)
 nba_projections <- import_projections()
@@ -16,8 +16,8 @@ projections_plus_salaries <- add_salary_ids(projections = augmented_projections,
 in_scope_projections <- scope_projections(projections = projections_plus_salaries)
 players_by_position <- split_by_position(in_scope_projections)
 filtered_players <- filter_players_by_position(player_position_list = players_by_position,
-                                               player_pts_rank = 10,
-                                               player_value_rank = 20,
+                                               player_pts_rank = 5,
+                                               player_value_rank = 5,
                                                player_pos_rank = 5)
 possible_lineups <- make_possible_lineups(filtered_players, salary_min = 40000)
 unique_lineups <- make_unique_lineups(possible_lineups)
@@ -30,10 +30,11 @@ optimized_lineups <- optimize_lineups(unique_lineup_object = augmented_unique_li
                                       limit_search = 25000,
                                       max_exposure = .65)
 
-entry_list <- import_entries("C:/Users/Jim/Documents/dfs/dk/nba/inputs/DKEntries_20180209_night.csv")
+entry_list <- import_entries("C:/Users/Jim/Documents/dfs/dk/nba/inputs/DKEntries_20180210_night.csv")
 setwd("C:/Users/Jim/Documents/dfs/dk/nba")
-export_lineups(lineups = optimized_lineups$lineups[c(1:40), ], 
+export_lineups(lineups = optimized_lineups$lineups, 
                entries = entry_list,
+               slate = "night",
                randomize_entries = FALSE)
 t2 <- Sys.time()
 t2-t1
