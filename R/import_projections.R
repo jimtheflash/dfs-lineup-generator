28fe40b7a9a90abe5c969d641f444a6f6bfb632b
anthonyshook <a-shook@northwestern.edu>
2018-02-12 23:48:46 -0600

    Added error reporting to import_projections

--- R/import_projections.R
+++ R/import_projections.R
@@ -13,14 +13,17 @@ import_projections <- function(source = "rotogrinders",
   
   import_loc <- paste0("https://rotogrinders.com/projected-stats/", sport, "-player.csv?site=", site)
 
-  projection_import <- read.table(import_loc, 
-                              sep = ",", 
-                              stringsAsFactors = FALSE)
+  projection_import <- try({read.table(import_loc, 
+                                         sep = ",", 
+                                         stringsAsFactors = FALSE)}, silent = TRUE)
+  
+  if (class(projection_import) == 'try-error'){
+    stop("There were no projections to be downloaded from ", import_loc)
+  }
   
   names(projection_import) <- c("player_name", "salary", "tm", "pos", "opp", "ceiling", "floor", "proj")
   
   projection_import <- dplyr::filter(projection_import, salary > 0)
-  
   return(projection_import)
   
-  }
+}
