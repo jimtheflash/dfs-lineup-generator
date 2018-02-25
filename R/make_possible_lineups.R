0db7526006cd09a4aa625b995cf99466c917116d
Jim <jim.kloet@gmail.com>
2018-02-24 18:38:34 -0600

    update R, not ready to go yet

--- R/make_possible_lineups.R
+++ R/make_possible_lineups.R
@@ -2,45 +2,87 @@
 #' 
 #' @param player_position_list The list of players and their positions
 #' @param salary_cap Upper limit for spend on a given lineup. Default is 50000
-#' @param salary_min Lower limit for spend on a given lineup. Default is 30000
+#' @param salary_min Lower limit for spend on a given lineup. Default is 49000
 #' 
 #' @return TBL of possible lineups
 #' 
 #' @details TODO: remove hard coding of positions
 #' 
 #' @export
-make_possible_lineups <- function(player_position_list, salary_cap = 50000, salary_min = 30000) {
+make_possible_lineups <- function(player_position_list, 
+                                  salary_cap = 50000, 
+                                  salary_min = 49000) {
   
-  possible_lineups <- expand.grid(lapply(player_position_list, 
-                                         function(x) unique(x$uid)), stringsAsFactors = FALSE) %>%
-    dplyr::filter(pg != sg & pg != pf & pg != sf & pg != `c` & pg != `f` & pg != `g` & pg != util &
-                    sg != pf & sg != sf & sg != `c` & sg != `f` & sg != `g` & sg != util &
-                    pf != sf & pf != `c` & pf != `f` & pf != `g` & pf != util &
-                    sf != `c` & sf != `f` & sf != `g` & sf != util &
-                    `c` != `f` & `c` != `g` & `c` != util &
-                    `f` != `g` & `f` != util &
-                    `g` != util)
+  # what do we want to do here? i think it's find all possible lineups for a set of players
+  browser()
   
-  # attach position salaries
-  for (i in names(player_position_list)) {
-    pos_df <- data.frame(uid = possible_lineups[[i]])
-    lu <- dplyr::select(player_position_list[[i]] %>% dplyr::ungroup(), uid, salary)
-    slry <- dplyr::left_join(pos_df, lu, by = "uid")
-    new_col <- paste0(i, "_salary")
-    possible_lineups[[new_col]] <- as.numeric(slry$salary)
+  position_list <- names(player_position_list)
+  
+  output_mat <- matrix(rep(0, length(position_list)), ncol = length(position_list))
+  output_df <- as.data.frame(output_mat)
+  names(output_df) <- position_list
+  
+  exhausted_positions <- c()
+  
+  for (p in position_list) {
+    
+    pos_df <- player_position_list[[p]]
+    
+    if (nrow(output_df) == 1) {
+      
+      output_df_expanded <- 
+        output_df[rep(seq_len(nrow(output_df)), 
+                      each = length(unique(pos_df$uid))), ]
+      output_df_expanded[[p]] <- unique(pos_df$uid)
+      
+      output_df <- output_df_expanded
+      
+      exhausted_positions[length(exhausted_positions) + 1] <- p
+      
+      next
+      
+    }
+
+    exhausted_cols <- as.data.frame(output_df[, names(output_df) %in% exhausted_positions])
+    names(exhausted_cols) <- exhausted_positions
+    
+    unique_combo_vectors <- as.character(apply(exhausted_cols, 1, function(x) paste(x, collapse = ";")))
+    
+    unique_new_uid <- unique(pos_df$uid)
+    
+    combined_raw <- expand.grid(static = unique_combo_vectors, dynamic = unique_new_uid,
+                                stringsAsFactors = FALSE)
+    
+    if (ncol(combined_raw) == 2) {
+      static_num <- as.numeric(combined_raw$static)
+      combined_mat <- matrix(nrow = length(static_num), ncol = 2)
+      combined_mat[, 1] <- static_num
+      combined_mat[, 2] <- combined_raw$dynamic
+      sort_vec <- apply(combined_mat, 1, sort)
+    }
+    
+    split_static <- strsplit(combined_raw$static, split = ";")
+    
+    split_static_mat <- do.call(rbind, split_static)
+    split_static_mat <- apply(split_static_mat, 2, as.numeric)
+    
+    combined_num_mat <- cbind(split_static_mat, as.numeric(combined_raw$dynamic))
+    
+    split_static_df <- as.data.frame(split_static_mat)
+    names(split_static_df) <- exhausted_positions
+    
+    combined_
+
+    exhausted_positions[length(exhausted_positions) + 1] <- p
+    
   }
   
-  # create a salary filter for valid lineups
-  salary_df <- dplyr::select(possible_lineups, dplyr::ends_with("_salary")) %>%
-    as.data.frame()
-  total_salary <- rowSums(salary_df, na.rm = TRUE)
-  possible_lineups$allpos_total_salary <- total_salary
+
+
+
+    
+    
+    
   
-  possible_lineups <- possible_lineups %>%
-    dplyr::filter(allpos_total_salary <= salary_cap & allpos_total_salary >= salary_min)
+  }
   
-  attr(possible_lineups, "out.attrs") <- NULL
-  
-  return(possible_lineups)
-  
-}
