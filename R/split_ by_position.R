52e223f4ab0231a25c10c08e6acead8e13388522
Jim <jim.kloet@gmail.com>
2018-02-26 19:42:02 -0600

    works, but possible lineups can take foreverrrrrr

--- R/split_ by_position.R
+++ R/split_ by_position.R
@@ -7,10 +7,8 @@
 #' @export
 split_by_position <- function(projections, entries, multiple_positions = c("util", "flex", "g", "f")) {
   
-  browser()
-  
   position_list <- entries %>%
-    dplyr::select(-ends_with("Fee"), -ends_with("ID"), -ends_with("Name")) %>%
+    dplyr::select(-dplyr::ends_with("Fee"), -dplyr::ends_with("ID"), -dplyr::ends_with("Name")) %>%
     names() %>%
     tolower()  
   
@@ -33,31 +31,37 @@ split_by_position <- function(projections, entries, multiple_positions = c("util
   special_pos <- names(player_by_position)[names(player_by_position) %in% multiple_positions]
   
   for (j in special_pos) {
-    if ()
+    if (j == "g") {
+      player_by_position$g <- rbind(player_by_position$pg, player_by_position$sg) %>%
+        dplyr::select(-player_position_rank) %>%
+        dplyr::group_by(player_name) %>%
+        dplyr::filter(row_number() == 1) %>%
+        dplyr::ungroup() %>%
+        dplyr::arrange(pts_rank) %>%
+        dplyr::mutate(player_position_rank = row_number()) 
+      next
+    }
+    
+    if (j == "f") {
+      player_by_position$f <- rbind(player_by_position$pf, player_by_position$sf) %>%
+        dplyr::select(-player_position_rank) %>%
+        dplyr::group_by(player_name) %>%
+        dplyr::filter(row_number() == 1) %>%
+        dplyr::ungroup() %>%
+        dplyr::arrange(pts_rank) %>%
+        dplyr::mutate(player_position_rank = row_number())
+      next
+    }
+    
+    if (j == "util") {
+      player_by_position$util <- projections %>%
+        dplyr::arrange(pts_rank) %>%
+        dplyr::mutate(player_position_rank = row_number())
+      next
+    }
   }
   
-  
-  # manually add the guard, forward, utility positions
-  player_by_position$g <- rbind(player_by_position$pg, player_by_position$sg) %>%
-    dplyr::select(-player_position_rank) %>%
-    dplyr::group_by(player_name) %>%
-    dplyr::filter(row_number() == 1) %>%
-    dplyr::ungroup() %>%
-    dplyr::arrange(pts_rank) %>%
-    dplyr::mutate(player_position_rank = row_number())
-  
-  player_by_position$f <- rbind(player_by_position$pf, player_by_position$sf) %>%
-    dplyr::select(-player_position_rank) %>%
-    dplyr::group_by(player_name) %>%
-    dplyr::filter(row_number() == 1) %>%
-    dplyr::ungroup() %>%
-    dplyr::arrange(pts_rank) %>%
-    dplyr::mutate(player_position_rank = row_number())
-  
-  player_by_position$util <- projections %>%
-    dplyr::arrange(pts_rank) %>%
-    dplyr::mutate(player_position_rank = row_number())
-  
   return(player_by_position)
+
   
 }
