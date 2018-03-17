5788561e0cea8c85d4c062f49a7247d02e35730d
Jim <jim.kloet@gmail.com>
2018-03-16 19:28:56 -0500

    update R to ensure that lineups have at least 2 games represented

--- R/augment_unique_lineups.R
+++ R/augment_unique_lineups.R
@@ -16,17 +16,20 @@ augment_unique_lineups <- function(unique_lineup_object,
     dplyr::group_by(uid) %>%
     dplyr::filter(row_number() == 1) %>%
     dplyr::ungroup() %>%
-    dplyr::select(uid, outcome)
+    dplyr::select(uid, game_id, outcome)
   projection_mat <- matrix(nrow = nrow(unique_lineup_object), ncol = ncol(unique_lineup_object))
+  game_mat <- matrix(nrow = nrow(unique_lineup_object), ncol = ncol(unique_lineup_object))
   for (i in 1:ncol(unique_lineup_object)) {
     uid_column <- data.frame(uid = unique_lineup_object[, i])
     uid_outcome <- dplyr::inner_join(uid_column, lu_outcome, by = "uid")
     projection_mat[, i] <- uid_outcome$outcome
+    game_mat[, i] <- uid_outcome$game_id
   }
   
   projections <- rowSums(projection_mat)
+  unique_games <- as.numeric(apply(game_mat, 1, function(x) length(unique(x))))
   
-  output_df <- data.frame(unique_lineup_object, outcome = projections)
+  output_df <- data.frame(unique_lineup_object, outcome = projections, games = unique_games)
   
   return(output_df)
   
