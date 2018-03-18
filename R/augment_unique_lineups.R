0266178ed4373b3f7b557f52a956210d6fe32452
Jim <jim.kloet@gmail.com>
2018-03-18 16:06:26 -0500

    updated lineup augmentation and optimization to enable enforcement of fanduel lineup rules, in this case the maximum number of players from a single nba team that can be represented across a single lineup

--- R/augment_unique_lineups.R
+++ R/augment_unique_lineups.R
@@ -12,24 +12,35 @@ augment_unique_lineups <- function(unique_lineup_object,
                                    outcome_object) {
   
   outcome_positions <- names(outcome_object)
+  
   lu_outcome <- do.call(rbind, outcome_object) %>%
     dplyr::group_by(uid) %>%
     dplyr::filter(row_number() == 1) %>%
     dplyr::ungroup() %>%
-    dplyr::select(uid, game_id, outcome)
+    dplyr::select(uid, tm, game_id, outcome)
+  
   projection_mat <- matrix(nrow = nrow(unique_lineup_object), ncol = ncol(unique_lineup_object))
+  
   game_mat <- matrix(nrow = nrow(unique_lineup_object), ncol = ncol(unique_lineup_object))
+  
+  tm_mat <- matrix(nrow = nrow(unique_lineup_object), ncol = ncol(unique_lineup_object))
+  
   for (i in 1:ncol(unique_lineup_object)) {
     uid_column <- data.frame(uid = unique_lineup_object[, i])
     uid_outcome <- dplyr::inner_join(uid_column, lu_outcome, by = "uid")
     projection_mat[, i] <- uid_outcome$outcome
     game_mat[, i] <- uid_outcome$game_id
+    tm_mat[, i] <- uid_outcome$tm
   }
   
   projections <- rowSums(projection_mat)
   unique_games <- as.numeric(apply(game_mat, 1, function(x) length(unique(x))))
+  max_players_per_team <- as.numeric(apply(tm_mat, 1, function(x) max(table(x))))
   
-  output_df <- data.frame(unique_lineup_object, outcome = projections, games = unique_games)
+  team_counter <- matrix(ncol = length(unique(lu_outcome$tm))) %>% as.data.frame()
+  names(team_counter) <- unique(lu_outcome$tm)
+  
+  output_df <- data.frame(unique_lineup_object, outcome = projections, games = unique_games, max_team_rep = max_players_per_team)
   
   return(output_df)
   
